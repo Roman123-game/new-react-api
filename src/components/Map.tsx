c3ff1e23d7da4f1043001cc5ffb41e1b104e412a
RB <82284311+Roman123-game@users.noreply.github.com>
2024-03-03 20:51:07 +0200

    update Map.css and Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,12 +5,10 @@ function Map(props:any) {
     const {position} = props
     return (  
 
-
-        <div className={`dot ${{position}}`}>.
-        
+        <div>
+        <div className={`dot ${position}`}>.</div>
         <h1 className="map">&#x1F5FA;</h1>
         </div>
-
 
 
     );
