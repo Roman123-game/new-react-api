6086a04509f3eb50b69bae33bee73de5427cc09c
romanbro <82284311+Roman123-game@users.noreply.github.com>
2024-06-22 10:20:35 +0300

    update Map.css and Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,10 +3,9 @@ import "./Map.css";
 function Map(props:any) {
     const {position} = props
     return (
-        <>
-         <h1 className="map">&#128506;</h1>
+         <h1 className="map">&#128506;
         <div data-title={position} className={`dot ${position}`}>&#x1F789;</div>
-        </>
+        </h1>
     );
 }
 
