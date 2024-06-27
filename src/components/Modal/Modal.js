a74979d9adb5afe90b22ae0f8229026b05d9dbe7
romanbro <82284311+Roman123-game@users.noreply.github.com>
2024-06-27 09:57:48 +0300

    update App.css, App.tsx and Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function Modal() {
+function Modal(props) {
+  const {text} =props
   return (
     <div className='modal'>
     <div className='modalContent'>
-<h1>modal</h1>
+<h1>{text}</h1>
     </div>
   </div>
   )
